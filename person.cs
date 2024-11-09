ec73d5393051612022d8be2a28b9439430b8751e
PIBO0123009 Половинкин Семён Андреевич <PIBO0123009@sibupk.su>
2024-11-09 14:48:05 +0700

    .

---
+++ person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _111
+{
+    class person
+    {
+        private string name;
+        private string familya;
+        private System.DateTime denzondenia;
+
+        public person()
+        {
+            name = "Andrey";
+            familya = "Tarasyinka";
+            denzondenia = new DateTime(2000, 10, 24);
+        }
+        public person(string n, string f, DataTime b)
+        {
+            Andrey = n;
+            Tarasyinka = s;
+            2000, 10, 24 = b;
+
+        }
+    }
+}
